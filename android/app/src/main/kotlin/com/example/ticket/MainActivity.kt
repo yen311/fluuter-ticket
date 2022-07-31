bd995f63073a9aaf815f34579a97c72c593cd1d2
Yen Kuo <u7075575@anu.edu.au>
2022-07-31 12:37:13 +0930

    init

---
+++ android/app/src/main/kotlin/com/example/ticket/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ticket
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My tickets"),
+      ),
+      body: const Center(
+        child: Text("My body"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 10,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.blueGrey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.airplane_ticket), label: "Ticket"),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: "Profile")
+        ],
+      ),
+    );
+  }
+}
